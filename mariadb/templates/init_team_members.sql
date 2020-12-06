2a780dac550bb948c5959a2e23d882364e315f54
ansible <ansible@prdx-ansible201.ziyotek2.local>
2020-12-06 18:58:48 -0500

    team2 final project

---
+++ mariadb/templates/init_team_members.sql
@@ -0,0 +1,13 @@
+create table team_members (member_id int NOT NULL, teamid int NOT NULL, member_name varchar(70) NOT NULL, member_state varchar(2) NOT NULL, primary key (member_id));
+
+insert into team_members values ('1','3','Lola Khabibullaeva','PA');
+insert into team_members values ('2','3','Kimberly Tyler','TX');
+insert into team_members values ('3','3','Anna Primkulova','VA');
+insert into team_members values ('4','3','Anugraha Kondian Rajaram','VA');
+insert into team_members values ('5','3','Feruza Akhmedova','PA');
+insert into team_members values ('6','3','Kathya Angel','VA');
+insert into team_members values ('7','3','Mukhayo Parpieva','FA');
+insert into team_members values ('8','3','Zarina Jurakulova','NC');
+
+
+
---
+++ mariadb/templates/init_teams.sql
@@ -0,0 +1,8 @@
+create table teams (team_id int NOT NULL, team_name varchar(70) NOT NULL, num_students int, primary key (team_id));
+
+insert into teams values ('1','Team Sudo','5');
+insert into teams values ('2','Devops Girls2020','7');
+insert into teams values ('3','Linux Geeks','5');
+insert into teams values ('4','Team Blue','7');
+insert into teams values ('5','DOS 2020','7');
+insert into teams values ('6','Slack-ish Penguins','4');
